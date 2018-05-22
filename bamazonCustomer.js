1de69452a3e4577183baa711383cff2f38aaef63
dolphinman <wpfan.org@gmail.com>
2018-05-22 07:36:01 -0500

    added wrong file last night, had it completed but uploaded wrong js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,7 +10,60 @@ var connection = mysql.createConnection({
     user: "root",
   
     // Your password
-    password: "",
-    database: "top_songsDB"
+    password: "root",
+    database: "bamazonDB"
   });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    readProducts();
+  });
+  
+  function readProducts() {
+    connection.query("SELECT items FROM products", function(err, res) {
+      if (err) throw err;
+    
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+  }
+
+  function createProduct() {
+    console.log("Inserting a new product...\n");
+    var query = connection.query(
+      "INSERT INTO products SET ?",
+      {
+        id: "1",
+        name: "Shampoo",
+        price: 8
+      },
+      function(err, res) {
+        console.log(res.affectedRows + " product inserted!\n");
+        // Call updateProduct AFTER the INSERT completes
+        updateProduct();
+      }
+    );
+  
+    // logs the actual query being run
+    console.log(query.sql);
+  }
+
+//   updates product codes
+  function updateProduct() {
+    console.log("Updating all product quantities...\n");
+    var query = connection.query(
+      "UPDATE products SET ? WHERE ?",
+      [
+        {
+          id: 1
+        },
+        {
+          name: "Shampoo"
+        },
+        {
+          flavor: "8"
+        }
+      ],
 

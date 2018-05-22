5d34782b10173b56908e09d2852455e6ed75e422
dolphinman <wpfan.org@gmail.com>
2018-05-21 21:41:56 -0500

    updated bamazon file

--- bamazon.sql
+++ bamazon.sql
@@ -4,13 +4,13 @@ CREATE database bamazonDB;
 USE bamazonDB;
 
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INT AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(50) NULL,
   department_name VARCHAR(50) NULL,
-  year INT NULL,
-  price DECIMAL(10,4) NULL,
+  price DECIMAL(10,2) NULL,
   stock_quantity VARCHAR(100) NULL,
   PRIMARY KEY (item_id)
 );
 
-SELECT * FROM top5000;
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES ("Bleach", "Cleaning", 7.95, 50),("Soap", "Cleaning", 2.95, 100), ("Paper_Towels", "Cleaning", 7.95, 50),("Batteries", "Electronics", 7.95, 50), ("TV", "Electronics", 7.95, 50), ("Gameboy", "Electronics", 7.95, 50), ("Shampoo", "Cleaning", 7.95, 50),("Conditioner", "Cleaning", 7.95, 50)

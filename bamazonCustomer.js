f12c46cc41e96a8cc8741b762a2cbd967270edda
dolphinman <wpfan.org@gmail.com>
2018-05-22 08:43:40 -0500

    updated js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,53 +17,74 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    // readProducts();
+    start();
   });
-  
-  function readProducts() {
-    connection.query("SELECT items FROM products", function(err, res) {
-      if (err) throw err;
-    
-      // Log all results of the SELECT statement
-      console.log(res);
-      connection.end();
-    });
-  }
 
-  function createProduct() {
-    console.log("Inserting a new product...\n");
-    var query = connection.query(
-      "INSERT INTO products SET ?",
-      {
-        id: "1",
-        name: "Shampoo",
-        price: 8
-      },
-      function(err, res) {
-        console.log(res.affectedRows + " product inserted!\n");
-        // Call updateProduct AFTER the INSERT completes
-        updateProduct();
+  // function which prompts the user for what action they should take
+function start() {
+  inquirer
+    .prompt({
+      name: "postOrBid",
+      type: "list",
+      message: "Welcome to bamazon. What product would you like to order?",
+      choices: ["1 Bleach $7.95", "2 Soap $2.95", "3 Paper Towels $7.95", "4 Batteries $7.95", "5 TV $7.95", "6 gameboy $7.95", "7 Shampoo $7.95", "8 Conditioner $7.95"]
+    })
+    .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.postOrBid.toLowerCase() === "POST") {
+      //   postAuction();
+      // }
+      // else {
+      //   bidAuction();
       }
-    );
+    });
+}
+
+// Still trying to get this to work //
+
+//   function readProducts() {
+//     connection.query("SELECT items FROM products", function(err, res) {
+//       if (err) throw err;
+    
+//       // Log all results of the SELECT statement
+//       console.log(res);
+//       connection.end();
+//     });
+//   }
+
+//   function createProduct() {
+//     console.log("Inserting a new product...\n");
+//     var query = connection.query(
+//       "INSERT INTO products SET ?",
+//       {
+//         id: "1",
+//         name: "Shampoo",
+//         price: 8
+//       },
+//       function(err, res) {
+//         console.log(res.affectedRows + " product inserted!\n");
+//         // Call updateProduct AFTER the INSERT completes
+//         updateProduct();
+//       }
+//     );
   
-    // logs the actual query being run
-    console.log(query.sql);
-  }
+//     // logs the actual query being run
+//     console.log(query.sql);
+//   }
 
-//   updates product codes
-  function updateProduct() {
-    console.log("Updating all product quantities...\n");
-    var query = connection.query(
-      "UPDATE products SET ? WHERE ?",
-      [
-        {
-          id: 1
-        },
-        {
-          name: "Shampoo"
-        },
-        {
-          flavor: "8"
-        }
-      ],
-
+// //   updates product codes
+//   function updateProduct() {
+//     console.log("Updating all product quantities...\n");
+//     var query = connection.query(
+//       "UPDATE products SET ? WHERE ?",
+//       [
+//         {
+//           id: 1
+//         },
+//         {
+//           name: "Shampoo"
+//         },
+//         {
+//           flavor: "8"
+//         }
+//       ]
